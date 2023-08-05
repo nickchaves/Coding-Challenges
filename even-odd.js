9101b73a523753ae84b94b3c829ce87510634c60
Nicholas Chaves <chavesndev@gmail.com>
2023-08-05 08:17:26 -0400

    added new challenge

---
+++ even-odd.js
@@ -0,0 +1,8 @@
+// Create a function that takes an integer as an argument and returns "Even" 
+// for even numbers or "Odd" for odd numbers.
+
+// My solution
+const evenOrOdd = num => num % 2 === 0 ? 'Even' : 'Odd';
+
+console.log(evenOrOdd(2));
+console.log(evenOrOdd(1));
