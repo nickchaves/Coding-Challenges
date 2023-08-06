1b0e76b0836622eedd96381c93a09ea161a32857
Nicholas Chaves <chavesndev@gmail.com>
2023-08-06 07:42:01 -0400

    added new challenge

---
+++ numToString.js
@@ -0,0 +1,14 @@
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+// My solution
+const numberToString = num => num.toString();
+
+console.log(numberToString(10));
+console.log(numberToString(37));
