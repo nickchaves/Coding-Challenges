7ad69e9ae514f167f7eb9c7642bfe64d8e77ce0f
Nicholas Chaves <chavesndev@gmail.com>
2023-08-07 08:02:36 -0400

    added new challenge

---
+++ 8kyu-opp.js
@@ -0,0 +1,13 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+// My solution
+const opposite = num => num * -1;
+
+console.log(opposite(1))
+console.log(opposite(-1))
